6ef15dab07cac2e0dcd2cfc4c2ac01bf3f1586fe
Duong Le <duongbk1602@gmail.com>
2022-12-02 21:26:08 +0700

    update logging (cont)

--- exp.py
+++ exp.py
@@ -190,7 +190,7 @@ class Exp:
 
         for index,pred in enumerate(preds[0]):
             output = np.squeeze(pred,axis = 0)
-            print(np.array(pred).shape)
+            print(np.array(pred).transpose(1,2,0))
             data = im.fromarray(np.squeeze(pred,0).transpose(1,2,0))
             data.save(os.path.join(folder_path,'pred_'+ index + '.png'))
 
