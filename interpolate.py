9e1b37fa0a712b1af269796d5ecece953e86649f
Duong Le <duongbk1602@gmail.com>
2022-12-03 01:49:23 +0700

    update validation

--- interpolate.py
+++ interpolate.py
@@ -40,6 +40,9 @@ def create_parser():
 def main():
     args = create_parser().parse_args()
 
+    if not args.resume_path:
+        raise ValueError('Missing location of pre-train model')
+
     exp = Exp(args)
     exp.interpolate()
 
